54076798907444b15dcbd52bfc3539356385f4c2
Eduardo Gomes <eduardo.go5577@gmail.com>
2026-06-16 17:32:52 -0300

    Implementação do projeto em arquitetura MVC

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
-package Ex11;
-import Ex11.Controller.*; 
-import Ex11.Repository.GerenciarFrota;
-import Ex11.View.ConsoleView;
+
+import Controller.*; 
+import Repository.GerenciarFrota;
+import View.ConsoleView;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Instanciamos o "Banco de Dados" (Repository)
+        
         GerenciarFrota repository = new GerenciarFrota();
         
-        // 2. Instanciamos a Interface de Usuário (View)
         ConsoleView view = new ConsoleView();
         
-        // 3. Instanciamos o Gerente passando os dois para ele (Controller)
         FrotaController controller = new FrotaController(view, repository);
         
-        // 4. Damos o Play!
         controller.iniciar();
     }
 }
